16cba0224e257f6f1e34feca4fd73f182327789a
MLjungg <mikael.lj@hotmail.com>
2019-03-29 09:26:45 +0100

    increased video size

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,9 +7,9 @@
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
-      <v-container fluid fill-height>
+      <v-container fluid>
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4 lg8 xl8>
+          <v-flex xs12 sm8 md4 lg12 xl12>
             <v-card class="elevation-14">
               <v-toolbar dark color="blue-grey darken-1">
                 <v-toolbar-title>Login</v-toolbar-title>
